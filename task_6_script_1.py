8b2fb8f2c767a0a3cc10f705191f195cfa0e39c4
python_user <not_def@mail.rr>
2022-12-09 20:14:27 +0300

    window not showing by thread bug corrected

--- task_6_script_1.py
+++ task_6_script_1.py
@@ -1,6 +1,7 @@
 # 6) Реализовать два небольших скрипта:
 # а) итератор, генерирующий целые числа, начиная с указанного,
 import itertools
+import os
 import time
 # Подсказка: использовать функцию count() и cycle() модуля itertools.
 # Обратите внимание, что создаваемый цикл не должен быть бесконечным.
@@ -17,20 +18,17 @@ import console_methods
 import window
 
 
-def generator_thread(first, second):
-    generator_integer = itertools.count(int(first))
+def generator_thread(argv):
+    generator_integer = itertools.count(int(argv[1]))
     if len(argv) > 2:
-        generator_integer = itertools.count(int(first))
-    # generator_integer = itertools.count(int(argv[1]))
-    # if len(argv) > 2:
-    #     generator_integer = itertools.count(int(argv[1]), int(argv[2]))
+        generator_integer = itertools.count(int(argv[1]))
 
     while True:
         dig = generator_integer.__next__()
         print(dig)
-        if second != None:
-            if int(second) == dig:
-                exit(0)
+        if len(argv)>2:
+            if int(argv[2]) == dig:
+                os._exit(0)
         time.sleep(2)
 
 
@@ -50,19 +48,7 @@ if __name__ == '__main__':
             print("Error. Second arg need digit")
             exit(1)
 
-    first = 0
-    second = 0
-    try:
-        first = argv[1]
-    except:
-        asd = 0
-
-    try:
-        second = argv[2]
-    except:
-        asd = 0
-
-    thread = Thread(target=generator_thread(first, second))
+    thread = Thread(target=generator_thread,args=(argv,))
     thread.start()
 
     window.show_first_window()
